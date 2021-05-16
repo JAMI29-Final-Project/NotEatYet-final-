748394203cedbcdb4bfa7791afb8122c44a0aacb
Edoardo Carollo <edoardocarollo1999@gmail.com>
2021-05-16 17:36:16 +0200

    Base SpringBoot

---
+++ src/main/java/it/noteatyertesting/testing/TestingApplication.java
@@ -0,0 +1,13 @@
+package it.noteatyertesting.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingApplication.class, args);
+	}
+
+}
