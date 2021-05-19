1dd1a335b29a61ad4f395c520335099aebedc3ab
giovannilamarmora <giovannilamarmora.working@gmail.com>
2021-05-19 14:48:35 +0200

    Ingredienti

--- src/main/java/it/noteatyertesting/testing/controllerDatiUser/ControllerIngredientiUser.java
+++ src/main/java/it/noteatyertesting/testing/controllerDatiUser/ControllerIngredientiUser.java
@@ -1,12 +1,11 @@
 package it.noteatyertesting.testing.controllerDatiUser;
 
 import it.noteatyertesting.testing.model.Ingrediente;
+import it.noteatyertesting.testing.model.Piatto;
 import it.noteatyertesting.testing.repository.IIngredientiCRUD;
+import it.noteatyertesting.testing.repository.IPiattiCRUD;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,8 +16,19 @@ public class ControllerIngredientiUser {
     @Autowired
     IIngredientiCRUD ingredientiGEST;
 
+    @Autowired
+    IPiattiCRUD piattiGEST;
+
     @GetMapping("/ingredientiuser/{piattoid}")
     List<Ingrediente> ingredienteList(@PathVariable int piattoid) {
         return ingredientiGEST.findIngredienteByPiattoId(piattoid);
+    }
+
+    @PostMapping("ingredientiuser/aggiungi/{idPiatto}")
+    public void addIngredientiAPiatto(@PathVariable int idPiatto, @RequestBody Ingrediente ingrediente) {
+        Piatto p = piattiGEST.findById(idPiatto).orElse(null);
+        ingrediente.setPiatto(p);
+        ingredientiGEST.save(ingrediente);
+
     }
 }
--- src/main/resources/static/user/aggiungiPiatto.js
+++ src/main/resources/static/user/aggiungiPiatto.js
@@ -1,6 +1,32 @@
 $(document).ready(function () {
     let idCategoria = -1;
     let idRistorante = -1;
+    let vettoreIngredienti1 = [];
+
+    $('#aggiungiIngrediente').click(function() {
+        const input = $('input#ingredienti');
+        const valore = input.val();
+        const ingrediente = {
+            "nome": valore
+        };
+        console.log(ingrediente);
+        if (valore) {
+            vettoreIngredienti1.push(ingrediente);
+            $('#listaIngredienti').append(`
+            <a class="list-group-item list-group-item-action list-group-item-warning" id="eliminaIngrediente">${ingrediente.nome}</a>`);
+        }
+        input.val('');
+        input.focus();
+    });
+    $('#listaIngredienti').on('click', '#eliminaIngrediente', function() { 
+        const nome = $(this).parent().attr('nome');
+        const ingrediente = {
+            "nome": nome
+        };
+        $(this).parent().remove();
+        const indiceIngrediente = vettoreIngredienti1.indexOf(ingrediente);
+        vettoreIngredienti1.splice(indiceIngrediente, 1);
+    });
 
     $('#aggiungiPiatto').click(function () {
         const piatto = {
@@ -13,25 +39,44 @@ $(document).ready(function () {
                 id: $('#ristorante').val(),
             }    
         }
+        let ingredienti = [];
+        vettoreIngredienti1.forEach(ingrediente => ingredienti.push(ingrediente));
+        const piattoIngre = {ingredienti};
         idCategoria = piatto.categoria.id;
         idRistorante = piatto.ristorante.id;
-        addPiatto(piatto);
+        if(piatto, piattoIngre) {
+            addPiatto(piatto, piattoIngre);
+        }
         
         $('#nomePiatto').val('');
         $('#prezzo').val('');
         $('#selectCategorie').val('');
         $('#ristorante').val('');
     })
-    function addPiatto(piatto){
-        console.log(piatto);
+    function addPiatto(piatto, piattoIngre){
+        console.log(piatto, piattoIngre);
         $.ajax({
             type: 'POST',
             url: `/user/piattiuser/aggiungi/${idRistorante}/${idCategoria}`,
-            data: JSON.stringify(piatto),
+            data: JSON.stringify(piatto, piattoIngre),
             contentType: 'application/json',
             dataType: 'json',
             error: function(response) {
-            console.log(response);
+                console.log("Risposta " + piatto.nome);
+                console.log("ID Piatto" + piatto.id);
+                idPiatto = +response.id;
+                for (let ingrediente of piattoIngre.ingredienti) {
+                    $.ajax({
+                        type: 'POST',
+                        url: `/user/ingredientiuser/aggiungi/${idPiatto}`,
+                        data: JSON.stringify(ingrediente),
+                        contentType: "application/json",
+                        dataType: 'json',
+                        success: function(data) {
+                            console.log(ingrediente);
+                        }
+                    });
+                }
             idCategoria = -1;
             idRistorante = -1;
                 Swal.fire({
