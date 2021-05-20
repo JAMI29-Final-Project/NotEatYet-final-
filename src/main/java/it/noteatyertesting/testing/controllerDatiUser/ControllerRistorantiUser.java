ff8e22736a2b687976a7774bff19ee7ec4237f7e
Edoardo Carollo <edoardocarollo1999@gmail.com>
2021-05-20 13:04:21 +0200

    modifica elimina immmagine

--- src/main/java/it/noteatyertesting/testing/controllerDatiUser/ControllerRistorantiUser.java
+++ src/main/java/it/noteatyertesting/testing/controllerDatiUser/ControllerRistorantiUser.java
@@ -92,6 +92,7 @@ public class ControllerRistorantiUser {
 
         Ristorante ristorante = getOne(id);
 
+        fs.deleteFile("./src/main/resources/static/upload"+ristorante.getImmagini());
         for (Piatto piattodel : ristorante.getMenu()) {
             for(Ingrediente ingrediente : piattodel.getIngredienti()) {
                 ingredientiGEST.deleteById(ingrediente.getId());
